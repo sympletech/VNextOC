7c23dcfe5f25654b5b2b5a750143cd00f8ee8109
Daniel Lewis <dewis@sympletech.com>
2013-05-29 20:23:11 -0700

    Pointing to HomePage now

--- VnextOC/HomePage.cs
+++ VnextOC/HomePage.cs
@@ -10,17 +10,9 @@ namespace VnextOC
     {
         public HomePage()
         {
-            Get["/"] = (dynamic _) => { return "Hello World!"; };
+            Get["/"] =  _ =>  View["HomePage"];
             
-            string s1 = "Sam";
-            var name = "Sam";
-            name.EndsWith("Hello");
-            var x  = new Dictionary<List<String>, Tuple<int, List<double>>>();
-            Dictionary<List<string>, Tuple<int, List<double>>> x2 = new Dictionary<List<String>, Tuple<int, List<double>>>();
-            dynamic s3 = "Sam";
-            s3.EndsWith("Hello");
-            s3.MaybeItShouldEndWith("uhoh");
-            int s4 = s3;
+           
 
         }
     }
